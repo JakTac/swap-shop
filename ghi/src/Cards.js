e1ca181bf11c51a29450dae8b9b4b8e10190e37a
Cody <codytorres59@gmail.com>
2023-01-10 17:22:35 -0500

    Working on API

--- ghi/src/Cards.js
+++ ghi/src/Cards.js
@@ -1,10 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+const apiUrl = "https://fakestoreapi.com/products";
+
 function Cards() {
+  //   const [loaded, setLoaded] = useState([]);
+  //   const [error, setError] = useState(false);
+
+  //   const LoadProducts = async () => {
+  //     const response = await fetch(
+  //       `$process.env.REACT_APP_swapshop_API_HOST}/https://fakestoreapi.com/products`
+  //     );
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setLoaded(data.loaded);
+  //     } else {
+  //       console.setError({ error: true });
+  //     }
+  //   };
+  //   useEffect(() => {
+  //     LoadProducts();
+  //   }, []);
+  const [productData, setProductData] = useState({});
+
+  useEffect(() => {
+    getProductUserWithFetch();
+  }, []);
+
+  const getProductUserWithFetch = async () => {
+    const response = await fetch(apiUrl);
+    const jsonData = await response.json();
+    setProductData(jsonData);
+  };
+
   return (
     <div className="card" style={{ width: "18rem" }}>
-      <img className="card-img-top" src="..." alt="Card image Kappa" />
+      <img
+        className="card-img-top"
+        src={productData.img}
+        alt="Card image Kappa"
+      />
       <div className="card-body">
-        <h5 className="card-title">Card TItle</h5>
-        <p className="card-text">PLease work and dont fail us</p>
+        <h5 className="card-title">{productData.name}</h5>
+        <p className="card-text">{productData.name}</p>
       </div>
       <ul className="list-group list-group-flush">
         <li className="list-group-item">This is a group uitem</li>
