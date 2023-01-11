fd018f905a8e848638c782aae4089294912d747f
Alonso Rodriguez <adrod0821@gmail.com>
2023-01-11 13:55:24 -0600

    filtered category search

--- swapshop/migrations/001_create_listing_and_acc_table.py
+++ swapshop/migrations/001_create_listing_and_acc_table.py
@@ -3,10 +3,10 @@ steps = [
         # "Up" SQL statement
         """
         CREATE TABLE listings (
-            id SERIAL PRIMARY KEY NOT NULL,
+            listings_id SERIAL PRIMARY KEY NOT NULL,
             image_url TEXT NOT NULL,
             name TEXT NOT NULL,
-            category TEXT NOT NULL,
+            category_id INT NOT NULL,
             condition TEXT NOT NULL,
             price FLOAT NOT NULL,
             description TEXT NOT NULL
@@ -31,6 +31,9 @@ steps = [
         """
         DROP TABLE accounts;
         """
-    ],
+    ]
+
+
+
 
 ]
