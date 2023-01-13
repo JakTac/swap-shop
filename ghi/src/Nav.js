274ef85e984da7690f7b4a7336af70475d021302
JakTac <jake.p.tippit@gmail.com>
2023-01-13 17:34:25 -0600

    Made some changes

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,9 +1,56 @@
 import { NavLink } from "react-router-dom";
 import { useToken } from "./Accounts/Token";
 
+function TokenHandler() {
+  const [token] = useToken()
+  if (token) {
+    return true
+  } else {
+    return false
+  }
+}
+
 function Nav() {
   const [token, , logout] = useToken()
-  return (
+  if (TokenHandler()) {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          SwapShop
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      &nbsp;&nbsp;&nbsp;
+      <button type="button" className="btn btn-light">
+        <NavLink
+          style={{ color: "black" }}
+          className="nav-link"
+          aria-current="page"
+          to="/createlisting/"
+        >
+          List Item
+        </NavLink>
+      </button>
+      &nbsp;&nbsp;&nbsp;
+      <button type="button" className="btn btn-light" onClick={logout}>
+          Logout
+      </button>
+      &nbsp;&nbsp;&nbsp;
+    </nav>
+  );
+  } else {
+    return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">
@@ -42,24 +89,10 @@ function Nav() {
           Login
         </NavLink>
       </button>
-      &nbsp;&nbsp;&nbsp;
-      <button type="button" className="btn btn-light">
-        <NavLink
-          style={{ color: "black" }}
-          className="nav-link"
-          aria-current="page"
-          to="/createlisting/"
-        >
-          List Item
-        </NavLink>
-      </button>
-      &nbsp;&nbsp;&nbsp;
-      <button type="button" className="btn btn-light" onClick={logout}>
-          Logout
-      </button>
-      &nbsp;&nbsp;&nbsp;
     </nav>
-  );
+    )
+  }
+  
 }
 
 export default Nav;
