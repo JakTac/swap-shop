6ade63c8b8ba6b521c29d2f892c6a279b717f511
Cody <codytorres59@gmail.com>
2023-01-19 06:41:51 -0500

    so i can pull from main

--- ghi/src/Listings/WomensPage.js
+++ ghi/src/Listings/WomensPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function WomensPage() {
   const [listings, setListings] = useState([]);
@@ -48,7 +50,7 @@ function WomensPage() {
           />
         </div>
       </div>
-      <div className="container">
+      {/* <div className="container">
         <div className="col-md-12 text-center">
           <table className="table table-striped">
             <thead>
@@ -83,7 +85,34 @@ function WomensPage() {
             </tbody>
           </table>
         </div>
-      </div>
+      </div> */}
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Body>
+           {listings
+                .filter((listing) =>
+                  listing.name.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((listing) => (
+                  <ListGroup className="list-group-flush">
+                  <ListGroup.Item>{listing.name}</ListGroup.Item>
+                  <ListGroup.Item>{listing.condition}</ListGroup.Item>
+                  <ListGroup.Item>{listing.price}</ListGroup.Item>
+                  <ListGroup.Item>{listing.description}</ListGroup.Item>
+                 </ListGroup>
+                ),
+
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
