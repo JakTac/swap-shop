02133a52e1f30d2a5180b1c7dba95cff60ff6dce
Ben <budemaso@gmail.com>
2023-01-24 17:30:59 -0700

    Unit testing, need to switch to class based

--- test_create_listing.py
+++ test_create_listing.py
@@ -1,11 +1,63 @@
-from roman_numerals import from_roman_to_arabic
+import { CreateListing } from "ghi/src/Listings/CreateListing";
+import { create } from "swapshop/queries/listings";
+import { create_listing } from "swapshop/routers/listings";
 
-def test_25():
-    # Arrange
-    input = "XXV"
+def test_image_url():
+# Arrange
+    input = image_url
 
-    # Act
-    result = from_roman_to_arabic(input)
+# Act
+    result = CreateListing.image_url(input)
 
-    # Assert
-    assert result == 25
+# Assert
+    assert result != None
+
+def test_name():
+# Arrange
+    input = name
+
+# Act
+    result = CreateListing.name(input)
+
+# Assert
+    assert result != None
+
+def category_id():
+# Arrange
+    input = category_id
+
+# Act
+    result = CreateListing.category_id(input)
+
+# Assert
+    assert result != None
+
+def test_condition():
+# Arrange
+    input = condition
+
+# Act
+    result = CreateListing.condition(input)
+
+# Assert
+    assert result != None
+
+def test_price():
+# Arrange
+    input = price
+
+# Act
+    result = CreateListing.price(input)
+
+# Assert
+    assert result != None
+
+def test_description():
+# Arrange
+    input = description
+
+# Act
+    result = CreateListing.description(input)
+
+# Assert
+    assert result != None
