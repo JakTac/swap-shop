4a6e3af147baea93fac3c4e0367eb88509e5197f
Ben <budemaso@gmail.com>
2023-01-25 11:31:09 -0700

    Can't seem to get 404 AssertionErrors in my unit tests resolved, still working it

--- swapshop/tests/test_create_listing.py
+++ swapshop/tests/test_create_listing.py
@@ -6,7 +6,7 @@ from routers.listings import ListingIn, ListingOut, ListingQueries
 from main import app
 
 
-client = TestClient(app=app)
+repo = TestClient(app=app)
 
 class ListingQueriesMock:
 
@@ -22,23 +22,42 @@ class ListingQueriesMock:
 
 def test_create_listing():
     # Arrange
+    app.dependency_overrides[ListingQueries] = ListingQueriesMock
+    listing_body = {
+        "image_url": "https://i.imgflip.com/1bmysm.jpg",
+        "name": "Never Gonna Give You Up",
+        "category_id": "2",
+        "condition": "Brand New",
+        "price": "500",
+        "description": "Never Gonna Let You Down",
+        "seller_id": "99",
+        "sold": False,
+    }
 
     # Act
+    result = repo.post("/createlisting/", json.dumps(listing_body))
 
     # Assert
+    assert result.status_code == 200
+    assert 1 == 1
 
     # A cleanup
     pass
 
 def test_get_listings():
     # Arrange
+    app.dependency_overrides[ListingQueries] = ListingQueriesMock
 
     # Act
+    result = repo.get("/mylistings/")
 
     # Assert
+    assert result.status_code == 200
+    assert result.json() == { "listings": [] }
+    assert 1 == 1
 
     # A cleanup
-    pass
+    app.dependency_overrides = {}
 
 def test_update_listing():
     # Arrange
