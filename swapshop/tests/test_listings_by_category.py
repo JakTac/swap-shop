5b20d1d80f9207177d07670ab84ef81f646f1c85
JakTac <jake.p.tippit@gmail.com>
2023-01-25 14:10:26 -0600

    Finished my unit test

--- swapshop/tests/test_listings_by_category.py
+++ swapshop/tests/test_listings_by_category.py
@@ -1,9 +1,19 @@
-import json
 from fastapi.testclient import TestClient
 from queries.listings import ListingQueries
-from routers.listings import ListingIn, ListingOut
-from authenticator import authenticator
 from main import app
 
+client = TestClient(app=app)
 
+class ListingQueriesMock:
+    def get_by_category(self, category_id: int):
+        return []
 
+def test_listings_by_category():
+    app.dependency_overrides[ListingQueries] = ListingQueriesMock
+    
+    res = client.get('listings/categories/1')
+
+    assert res.status_code == 200
+    assert res.json() == []
+
+    app.dependency_overrides = {}
