21ab35ef602ee99bb7061ba51c87cb3c0e6c6057
Ben <budemaso@gmail.com>
2023-01-27 11:44:29 -0700

    Troubleshoot CI/CD issues, added 'get' and () instead of [] for our DATABASE_URL

--- swapshop/queries/accounts.py
+++ swapshop/queries/accounts.py
@@ -3,7 +3,7 @@ import os
 from psycopg_pool import ConnectionPool
 
 
-pool = ConnectionPool(conninfo=os.environ.get["DATABASE_URL"])
+pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL"))
 
 
 class Error(BaseModel):
