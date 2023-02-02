5a439410425b55755b730ef9b6b0d3d12b3f99d2
Alonso Rodriguez <adrod0821@gmail.com>
2023-02-02 17:54:20 -0600

    prettier fix

--- ghi/src/Listings/CreateListing.js
+++ ghi/src/Listings/CreateListing.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useAuthContext } from "../Accounts/Token";
 
 function CreateListing(list) {
   const [listData, setListData] = useState({
